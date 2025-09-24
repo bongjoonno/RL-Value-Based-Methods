d2e3f5cdf5b685cdb5542c1ad98e768659a1d029
bongjoonno <zachary.feryate@gmail.com>
2025-09-24 23:06:33 +0000

    using cProfile and pstats to find out which functions are bottlenecking

--- imports.py
+++ imports.py
@@ -3,5 +3,5 @@ import numpy as np
 import time
 import pickle
 import time
-
+import cProfile, pstats
 import matplotlib.pyplot as plt
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from imports import cProfile, pstats
+
 from constants import LEARNING_METHODS
 
 from test_learning_methods import test_learning_methods
 
-methods_accuracy_dict = test_learning_methods(LEARNING_METHODS, 10, 10)
 
-for method, accuracy in methods_accuracy_dict.items():
-    print(method, sum(accuracy) / len(accuracy))
+def main():
+    methods_perfect_accuracy_dict = test_learning_methods(LEARNING_METHODS, 2, 3)
+
+    methods_accuracy_percentage_dict = {}
+
+    for method, accuracy in methods_perfect_accuracy_dict.items():
+        methods_accuracy_percentage_dict[method] =  sum(accuracy) / len(accuracy)
+
+    return methods_accuracy_percentage_dict
+
+if __name__ == '__main__':
+    profiler = cProfile.Profile()
+    
+    profiler.enable()
+    methods_accuracy_percentage_dict = main()
+    profiler.disable()
+    
+    #for method, accuracy in methods_accuracy_percentage_dict.items():
+        #print(f"{method} accuracy: {accuracy*100:.2f}%")
+    
+
+    stats = pstats.Stats(profiler).sort_stats("cumulative")
+    stats.print_stats()
