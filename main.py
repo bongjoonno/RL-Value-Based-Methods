89b690492e8e040301215eecada4b9518f9d458b
bongjoonno <zachary.feryate@gmail.com>
2025-10-01 15:21:20 -0700

    wrote welcome message for user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from train_test import train_test
-from get_inputs import get_dimensions_input, get_learning_methods
+from utils.get_inputs import get_dimensions_input, get_learning_methods
+from visuals import welcome_message
 
 def main(learning_method, course_length_y, course_length_x):
     has_perfect_accuracy = train_test(course_length_y, course_length_x, learning_method, display_episode_path = True)
@@ -7,6 +8,7 @@ def main(learning_method, course_length_y, course_length_x):
     return "Perfect path found!" if has_perfect_accuracy else "Perfect path not found..."
 
 if __name__ == '__main__':
+    welcome_message()
     y, x = get_dimensions_input()
     learning_method = get_learning_methods()
     methods_accuracy_percentage_dict = main(learning_method, y, x)
--- visuals.py
+++ visuals.py
@@ -4,3 +4,14 @@ def clear_board_in_place(board_y_length):
     for _ in range(board_y_length):
                     sys.stdout.write("\033[F")
                     sys.stdout.write("\033[K")
+
+def welcome_message():
+    print("HELLO AND WELCOME TO THE REINFORCEMENT LEARNING GRID GAME")
+    
+    print("THE AI'S GOAL IS TO CONTROL PLAYER 'P' TO REACH THE END AT '1' ")
+    
+    print('[P 0 0 0 0 0]\n'
+          '[0 0 0 0 0 0]\n'
+          '[0 0 0 0 0 1]\n')
+    
+    print("(p.s. the AI can move WASD)")
