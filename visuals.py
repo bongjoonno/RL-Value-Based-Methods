0757df0f6c436b2f38bd9d01c42719a3cec507e0
bongjoonno <zachary.feryate@gmail.com>
2025-10-01 15:24:00 -0700

    fixed visuals to include more new lines, next is input validation for dimensions

--- visuals.py
+++ visuals.py
@@ -6,12 +6,12 @@ def clear_board_in_place(board_y_length):
                     sys.stdout.write("\033[K")
 
 def welcome_message():
-    print("HELLO AND WELCOME TO THE REINFORCEMENT LEARNING GRID GAME")
+    print("HELLO AND WELCOME TO THE REINFORCEMENT LEARNING GRID GAME\n")
     
-    print("THE AI'S GOAL IS TO CONTROL PLAYER 'P' TO REACH THE END AT '1' ")
+    print("THE AI'S GOAL IS TO CONTROL PLAYER 'P' TO REACH THE END AT '1'\n")
     
     print('[P 0 0 0 0 0]\n'
           '[0 0 0 0 0 0]\n'
           '[0 0 0 0 0 1]\n')
     
-    print("(p.s. the AI can move WASD)")
+    print("(p.s. the AI can move WASD)\n")
