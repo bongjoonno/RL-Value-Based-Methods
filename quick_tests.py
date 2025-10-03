82a9d1f8e28a6ea095df7dfc73d20017c3dd4802
bongjoonno <zachary.feryate@gmail.com>
2025-10-02 19:23:13 -0700

    testing all learning methods, messed up script

--- quick_tests.py
+++ quick_tests.py
@@ -2,19 +2,6 @@ from constants import LEARNING_METHODS_LIST
 from test_learning_methods import test_learning_methods
 from imports import sys, time
 
-#res = test_learning_methods(LEARNING_METHODS_LIST, 10, 10)
+res = test_learning_methods(LEARNING_METHODS_LIST, 3, 3)
 
-#print(res)
-
-lst = [[0, 0, 0],
-       [0, 0, 0],
-       [0, 0, 0]]
-
-for lt in lst:
-    print(lt)
-
-time.sleep(1)
-    
-for _ in range(len(lst)):
-    sys.stdout.write("\033[F")  # move cursor up one line
-    sys.stdout.write("\033[K")
+print(res)
