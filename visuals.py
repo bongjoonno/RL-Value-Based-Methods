ac75143ed114bd7725eb8e9cef5583d1e45c7ecc
bongjoonno <zachary.feryate@gmail.com>
2025-10-02 19:27:13 -0700

    add exclamation marks to welcome massage

--- visuals.py
+++ visuals.py
@@ -6,9 +6,9 @@ def clear_board_in_place(board_y_length):
                     sys.stdout.write("\033[K")
 
 def welcome_message():
-    print("HELLO AND WELCOME TO THE REINFORCEMENT LEARNING GRID GAME\n")
+    print("HELLO AND WELCOME TO THE REINFORCEMENT LEARNING GRID GAME!!!\n")
     
-    print("THE AI'S GOAL IS TO CONTROL PLAYER 'P' TO REACH THE END AT '1'\n")
+    print("THE AI'S GOAL IS TO CONTROL PLAYER 'P' TO REACH THE END AT '1'!!!\n")
     
     print('[P 0 0 0 0 0]\n'
           '[0 0 0 0 0 0]\n'
