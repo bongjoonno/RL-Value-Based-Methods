a122c68cd97a9f6f9ccb4b376949f99891de7cd3
bongjoonno <zachary.feryate@gmail.com>
2025-10-10 23:30:28 -0700

    finished, just changed to q-learning, all the others suck

--- train_test.py
+++ train_test.py
@@ -18,7 +18,7 @@ def train_test(
 ) -> bool:
 
     testing_trial_limit = (course_length_y - 1) + (course_length_x - 1)
-    training_trial_limit = TRAIN_FACTOR * (course_length_y * course_length_x)
+    training_trial_limit = int((course_length_y * course_length_x)** TRAIN_FACTOR)
     
     q_scores = gen_q_table(course_length_y, course_length_x)
 
--- utils/discounted_reward.py
+++ utils/discounted_reward.py
@@ -1,4 +1,8 @@
-def get_discounted_reward(rewards, gamma, Gt_reward=0, discounted_rewards=[]):
+from imports import sys
+
+sys.setrecursionlimit(100_000)
+
+def get_discounted_reward(rewards, gamma, Gt_reward=0, discounted_rewards=[]):  
     if not rewards: return discounted_rewards[::-1]
     
     Gt_reward = Gt_reward * gamma + rewards[-1]
