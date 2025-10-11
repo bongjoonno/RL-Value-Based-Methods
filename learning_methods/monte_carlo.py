ec29b23c271c9ce7b25554a4bdeb07619de1a88b
bongjoonno <zachary.feryate@gmail.com>
2025-10-10 21:35:02 -0700

    updated monte_carlo function to stop when it goes 1 episode over the total amount of epochs

--- learning_methods/monte_carlo.py
+++ learning_methods/monte_carlo.py
@@ -2,9 +2,10 @@ from board import Board
 from utils.discounted_reward import get_discounted_reward
 from constants import ALPHA, GAMMA
 
-def monte_carlo(
-    board: Board, 
-    train_trial_limit:int , 
+
+def monte_carlo_update(
+    board: Board,
+    train_trial_limit: int,
     alpha = ALPHA, 
     gamma = GAMMA
 ) -> None:
@@ -14,10 +15,11 @@ def monte_carlo(
         outcome = board.perform_move()
         cur_action = board.chosen_action
 
-        if outcome in ['finished course', 'Ran out of trials']: break
+        if outcome in ['finished course', 'Ran out of trials']:
+            break
 
     rewards = list(board.trajectories['reward'])
-    discounted_rewards = get_discounted_reward(rewards, gamma, Gt_reward = 0, discounted_rewards = [])
+    discounted_rewards = get_discounted_reward(rewards, GAMMA, Gt_reward = 0, discounted_rewards = [])
 
     for i in range(len(board.trajectories['state'])):
         cur_state = board.trajectories['state'][i]
@@ -27,3 +29,17 @@ def monte_carlo(
         q = board.q_table[cur_state][cur_action]
 
         board.q_table[cur_state][cur_action] += alpha * (target - q)
+    
+def monte_carlo(course_length_y, course_length_x, epochs, q_table, train_trial_limit, epsilon):
+    board = Board(course_length_y, course_length_x, q_table, trial_limit = train_trial_limit, epsilon = epsilon)
+    
+    while epochs > 0:
+        monte_carlo_update(board)
+        
+        epsilon = max(0.01, epsilon * 0.999)
+        q_table = board.q_table
+        epochs -= board.move_number
+        
+        board = Board(course_length_y, course_length_x, q_table, trial_limit = train_trial_limit, epsilon = epsilon)
+        print(epochs)
+    return q_table
